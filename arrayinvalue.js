78c344753300c3806dff75d8f6bf9660785874e6
arunclenton <arunclenten@gmail.com>
2022-06-23 19:25:49 +0530

    create

--- arrayinvalue.js
+++ arrayinvalue.js
@@ -1,8 +1,7 @@
-let arr;
+let arr,sum ;
 arr=[10,20,5,2,8];
-for(i=0;i<arr.length;i++){
-    
-   
-
+sum=0;
+for(i=0; i<arr.length;i++){ 
+    sum += arr[i];
 }
-console.log(arr[+i]);
+console.log(sum);
